fc8e3e5886cb51bc86909b0623e2685e48555103
Jon Harrison <jonathan_harrison@hotmail.com>
2022-11-15 20:18:31 +0000

    add solution with array of objects and ternary operator for console.log

--- 05-advanced-javascript-module/01-objects-lesson/activities/02-Stu-Objects/solved/script.js
+++ 05-advanced-javascript-module/01-objects-lesson/activities/02-Stu-Objects/solved/script.js
@@ -1,1 +1,26 @@
-//WRITE YOUR CODE BELOW
+var customerOrder = [
+    {
+        name : "Coffee",
+        numberOfSugars : 1,
+        isReady : true
+    },
+    {
+        name : "Tea",
+        numberOfSugars : 2,
+        isReady : false
+    },
+    {
+        name : "Hot chocolate",
+        numberOfSugars : 0,
+        isReady : true
+    }
+    ];
+
+for(var order of customerOrder)
+{
+    console.log("Customer order \n" + 
+                "Drink  : " + order.name + "\n" +
+                "Sugars : " + order.numberOfSugars + "\n" +
+                "Status : " + (order.isReady ? "Ready for pick-up" : "Still in order queue")
+    );
+}
