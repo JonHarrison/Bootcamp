7f070ef6dc0ae5469a6cdd2cbd64a0947c0d6ab7
Jon Harrison <jonathan_harrison@hotmail.com>
2022-11-15 20:40:56 +0000

    added solution

--- 05-advanced-javascript-module/01-objects-lesson/activities/04-Stu-Object-Methods/solved/script.js
+++ 05-advanced-javascript-module/01-objects-lesson/activities/04-Stu-Object-Methods/solved/script.js
@@ -1,1 +1,10 @@
-//WRITE YOUR CODE BELOW
+var menu = {
+    "coffee": 1.60,
+    "tea": 1.50,
+    "hot chocolate": 2,
+    "milk": 0.5,
+    "squash" : 0.3
+};
+
+console.log("Drink names : ", Object.keys(menu));
+console.log("Menu total: £" + Object.values(menu).reduce((a,b) => { return a + b; }).toFixed(2));
