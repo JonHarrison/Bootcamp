c38b74723d9f7b3863a52e16c509dd4f4bb9d2ee
Jon Harrison <jonathan_harrison@hotmail.com>
2022-11-16 19:55:20 +0000

    add solution

--- 05-advanced-javascript-module/02-functions-lesson/activities/03-Stu-Functions/solved/script.js
+++ 05-advanced-javascript-module/02-functions-lesson/activities/03-Stu-Functions/solved/script.js
@@ -1,1 +1,17 @@
-// Write Your Code Below
+function addOneAndTwo() { 
+    return 1+2;
+}
+
+var subtractTenFromTwenty = function() {
+    return 20-10;
+}
+
+function areaOfASquare(side) {
+    return side * side;
+}
+
+console.log("addOneAndTwo returns " + addOneAndTwo());
+console.log("subtractTenFromTwenty returns " + subtractTenFromTwenty());
+console.log("areaOfASquare(2) returns " + areaOfASquare(2));
+console.log("areaOfASquare(10) returns " + areaOfASquare(10));
+
