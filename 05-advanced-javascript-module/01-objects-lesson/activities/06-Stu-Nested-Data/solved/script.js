916c93c86c4886eabbe85d0a48cdc5a8875644be
Jon Harrison <jonathan_harrison@hotmail.com>
2022-11-16 18:30:00 +0000

    added extra drink options

--- 05-advanced-javascript-module/01-objects-lesson/activities/06-Stu-Nested-Data/solved/script.js
+++ 05-advanced-javascript-module/01-objects-lesson/activities/06-Stu-Nested-Data/solved/script.js
@@ -1,20 +1,35 @@
-
-var sizeOptions = { small:0, medium:1, large:2, xlarge:3 };
-var milkOptions = { regular:0, "non-fat":1, "lactose-free":2 };
-var icedOptions = { yes:0, no:1 };
-var sugarOptions = { yes:0, no:1 };
+var sizeOptions = { small: 0, medium: 1, large: 2, "x-large": 3 };
+var milkOptions = { regular: 0, "non-fat": 1, "lactose-free": 2 };
+var icedOptions = { yes: 0, no: 1 };
+var sugarOptions = { yes: 0, no: 1 };
 
 var menu = {
     milkOptions: milkOptions,
     sizeOptions: sizeOptions,
-    foodOptions: {
-    },
+    foodOptions: [
+    ],
     drinkOptions: [
         {
-            name: "coffee",
+            name: "small coffee",
             price: 1.20,
             size: sizeOptions.small,
+            milk: milkOptions.regular,
+            iced: icedOptions.no,
+            sugar: sugarOptions.no
+        },
+        {
+            name: "medium lactose free coffee",
+            price: 1.60,
+            size: sizeOptions.medium,
             milk: milkOptions["lactose-free"],
+            iced: icedOptions.no,
+            sugar: sugarOptions.no
+        },
+        {
+            name: "large non-fat coffee",
+            price: 2.10,
+            size: sizeOptions.large,
+            milk: milkOptions["non-fat"],
             iced: icedOptions.no,
             sugar: sugarOptions.no
         },
@@ -27,10 +42,10 @@ var menu = {
             sugar: sugarOptions.yes
         }
     ]
- };
+};
 
 for (var drink of menu.drinkOptions) {
-    console.log("The price of a " + drink.name + " is £" + drink.price.toFixed(2) + " , the price of two " + drink.name + "'s is £" + (drink.price*2).toFixed(2));
+    console.log("The price of a " + drink.name + " is £" + drink.price.toFixed(2) + " , the price of two " + drink.name + "'s is £" + (drink.price * 2).toFixed(2));
 }
 console.log("milk options", menu.milkOptions);
 console.log("size options", menu.sizeOptions);
