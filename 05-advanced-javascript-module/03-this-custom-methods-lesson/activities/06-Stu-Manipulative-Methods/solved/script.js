61d7f79bce2824a676dd6851a9a927d018df8280
Jon Harrison <jonathan_harrison@hotmail.com>
2022-11-18 09:41:14 +0000

    add solution

--- 05-advanced-javascript-module/03-this-custom-methods-lesson/activities/06-Stu-Manipulative-Methods/solved/script.js
+++ 05-advanced-javascript-module/03-this-custom-methods-lesson/activities/06-Stu-Manipulative-Methods/solved/script.js
@@ -2,4 +2,15 @@ var constellations = ["Orion", "Scorpius", "Lyra", "Ursa Major", "Ursa Minor"];
 var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 var star = "polaris";
 
-// WRITE YOUR CODE BELOW
+console.log("constellations ", constellations);
+constellations.unshift("Canis Major");
+console.log("constellations ", constellations);
+console.log("planets", planets);
+planets.pop(); // remove last element which is Venus
+console.log("planets", planets);
+var galaxy = constellations.concat(planets);
+console.log("galaxy", galaxy);
+console.log("star " + star);
+var ucStar = star.toUpperCase();
+console.log("uppercase star " + ucStar);
+
