ebca92833154605be3f2e26a706e1617654433dc
Jon Harrison <jonathan_harrison@hotmail.com>
2022-11-18 09:39:31 +0000

    add solution

--- 05-advanced-javascript-module/03-this-custom-methods-lesson/activities/04-Stu-Object-This/solved/script.js
+++ 05-advanced-javascript-module/03-this-custom-methods-lesson/activities/04-Stu-Object-This/solved/script.js
@@ -1,13 +1,13 @@
-console.log(this);
+console.log(this); // this has global scope
 
 function helloThis() {
-  console.log('Inside this function, this is ' + this);
+  console.log('Inside this function, this is ' + this); // this has global scope
 }
 
 var child = {
   age: 10,
   ageTenYears: function () {
-    console.log(this.age + 10);
+    console.log(this.age + 10); // this has scope of the child object
   },
 };
 
@@ -16,7 +16,11 @@ var investor = {
   investment: {
     initialInvestment: 5000,
     investmentGrowth: function () {
-      console.log(this.initialInvestment * 1.15);
+      console.log(this.initialInvestment * 1.15); // this has scope of the investment object
     },
   },
 };
+
+helloThis();
+child.ageTenYears();
+investor.investment.investmentGrowth();
