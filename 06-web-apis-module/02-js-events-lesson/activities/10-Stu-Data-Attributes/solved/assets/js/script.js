7387efd5e15e4746b26e31c81892d3cb3d61a17d
Jon Harrison <jonathan_harrison@hotmail.com>
2022-12-01 17:49:45 +0000

    add solution

--- 06-web-apis-module/02-js-events-lesson/activities/10-Stu-Data-Attributes/solved/assets/js/script.js
+++ 06-web-apis-module/02-js-events-lesson/activities/10-Stu-Data-Attributes/solved/assets/js/script.js
@@ -3,5 +3,14 @@ var container = document.querySelector(".container");
 container.addEventListener("click", function(event) {
   var element = event.target;
 
-  // TODO: Complete function
+  switch (element.getAttribute("data-state")) {
+    case "hidden":
+      element.setAttribute("data-state", "visible");
+      element.textContent = element.getAttribute("data-number");
+      break;
+    case "visible":
+      element.setAttribute("data-state", "hidden");
+      element.textContent = "";
+      break;  
+  }
 });
