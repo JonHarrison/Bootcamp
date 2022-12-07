086e257badaa82353a74c02143fc527edc60b9da
Jon Harrison <jonathan_harrison@hotmail.com>
2022-12-07 13:30:40 +0000

    add solution

--- 07-third-party-apis-module/01-jquery-lesson/activities/10-Stu-Event-Delegation/solved/assets/js/script.js
+++ 07-third-party-apis-module/01-jquery-lesson/activities/10-Stu-Event-Delegation/solved/assets/js/script.js
@@ -29,7 +29,12 @@ function handleFormSubmit(event) {
 }
 
 // TODO: Create a function to handle removing a list item when `.delete-item-btn` is clicked
+function removeListItem(event) {
+  var btnClicked = $(event.target);
+  btnClicked.parent('li').remove(); // remove this item from it's parent
+}
 
 // TODO: Use event delegation and add an event listener to `shoppingListEl` to listen for a click event on any element with a class of `.delete-item-btn` and execute the function created above
+shoppingListEl.on('click','.delete-item-btn',removeListItem);
 
 shoppingFormEl.on('submit', handleFormSubmit);
