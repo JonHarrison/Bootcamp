f1160845a757b4ca606271e48a146ea2264bea0c
Jon Harrison <jonathan_harrison@hotmail.com>
2022-12-07 18:21:04 +0000

    add O to block X from winning

--- 07-third-party-apis-module/01-jquery-lesson/activities/08-Stu-Dom-Traversal/solved/assets/js/script.js
+++ 07-third-party-apis-module/01-jquery-lesson/activities/08-Stu-Dom-Traversal/solved/assets/js/script.js
@@ -5,4 +5,4 @@ var rootEl = $('#root');
 rootEl.children('ul').children().css('background-color','white');
 
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
-rootEl.children('ul').eq(2).children('li').eq(0).text('X'); // add X in 3rd row (ul) then 1st column (li)
+rootEl.children('ul').eq(2).children('li').eq(0).text('O'); // add X in 3rd row (ul) then 1st column (li)
