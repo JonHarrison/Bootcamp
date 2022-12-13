c875c3e0816d988f1422e664bc087511613c485f
Jon Harrison <jonathan_harrison@hotmail.com>
2022-12-13 15:51:42 +0000

    add solution

--- 08-server-apis-module/01-api-and-ajax-lesson/activities/04-Stu-AJAX-HTML/solved/script.js
+++ 08-server-apis-module/01-api-and-ajax-lesson/activities/04-Stu-AJAX-HTML/solved/script.js
@@ -5,14 +5,46 @@ var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 $.ajax({
   url: queryURL,
   method: "GET"
-}).then(function(response) {
+}).then(function (response) {
+  console.log(response);
 
   // Create and save a reference to new empty table row
+  var newTr = $('<tr>');
   // Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
+  var tdTitle = $('<td>').text(response.Title);
+  var tdYear = $('<td>').text(response.Year);
+  var tdActors = $('<td>').text(response.Actors);
   // Append the td elements to the new table row
+  newTr.append(tdTitle, tdYear, tdActors);
   // Append the table row to the tbody element
+  $('tbody').append(newTr);
 });
 
 // Repeat the above logic to add rows for two more movies of your choice
-
+displayMovieDetails('Labyrinth');
+displayMovieDetails('2001');
 // BONUS: Once complete, try to make your code as DRY as possible through the use of functions
+function displayMovieDetails(movie) {
+  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+  $.ajax({
+    type: 'GET', url: queryURL,
+    success: function(response, text) {
+      console.log(response);
+  
+      // Create and save a reference to new empty table row
+      var newTr = $('<tr>');
+      // Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
+      var tdTitle = $('<td>').text(response.Title);
+      var tdYear = $('<td>').text(response.Year);
+      var tdActors = $('<td>').text(response.Actors);
+      // Append the td elements to the new table row
+      newTr.append(tdTitle, tdYear, tdActors);
+      // Append the table row to the tbody element
+      $('tbody').append(newTr);
+    },
+    error: function(request, status, error) {
+      alert(request.responseText);
+    }
+  });
+}
