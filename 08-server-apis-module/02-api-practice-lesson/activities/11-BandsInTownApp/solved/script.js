a4addbfa9f90a269038720cc1ae35c25a747c825
Jon Harrison <jonathan_harrison@hotmail.com>
2022-12-15 18:57:38 +0000

    add solution

--- 08-server-apis-module/02-api-practice-lesson/activities/11-BandsInTownApp/solved/script.js
+++ 08-server-apis-module/02-api-practice-lesson/activities/11-BandsInTownApp/solved/script.js
@@ -1,18 +1,26 @@
 function searchBandsInTown(artist) {
   // Add code to query the bands in town api searching for the artist received as an argument to this function
-  var queryURL = `https://rest.bandsintown.com/artists/${artist}?app_id=aaaaaa`;
+  const APP_ID = 'trilogy'; // codingbootcamp
+  var queryURL = `https://rest.bandsintown.com/artists/${artist}?app_id=${APP_ID}`;
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
     console.log(response);
     // Using jQuery, append the following to the #artist-div :
-    // The artist's name
-    // The artists thumbnail image
-    // The number of fans tracking this artist
-    // The number of upcoming events for this artist
-    // A link to the bandsintown url for this artist
-    // Note: Append actual HTML elements, not just text
+    $('#artist-div').append([
+      // The artist's name
+      $('<p>', { text: response.name }),
+      // The artists thumbnail image
+      $('<img>', { src: response.thumb_url }),
+      // The number of fans tracking this artist
+      $('<p>', { text: response.fanTrack }),
+      // The number of upcoming events for this artist
+      $('<p>', { text: response.upcoming_event_count }),
+      // A link to the bandsintown url for this artist
+      $('<a>', { href: response.url, text: response.url }),
+      // Note: Append actual HTML elements, not just text
+    ]);
   });
 }
 
