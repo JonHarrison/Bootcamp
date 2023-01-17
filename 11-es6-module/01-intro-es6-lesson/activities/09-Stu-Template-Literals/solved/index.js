baa11fcade540fc3ebcc575eedc7594cb81ea858
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-17 20:42:31 +0000

    destructure music to save music.

--- 11-es6-module/01-intro-es6-lesson/activities/09-Stu-Template-Literals/solved/index.js
+++ 11-es6-module/01-intro-es6-lesson/activities/09-Stu-Template-Literals/solved/index.js
@@ -7,11 +7,12 @@ const music = {
 
 // Write code between the <div> tags to output the data from the music object above.
 // Use an h2 element for the title and a p element for artist and title
+const { title, artist, album } = music;
 const songSnippet = `
   <div class="song">
-    <h2>${music.title}</h2>
-    <p>${music.artist}</p>
-    <p>${music.album}</p>
+    <h2>${title}</h2>
+    <p>${artist}</p>
+    <p>${album}</p>
   </div>
 `;
 
