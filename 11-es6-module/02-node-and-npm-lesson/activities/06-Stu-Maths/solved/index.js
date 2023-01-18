0f90eb30a0500c5d531f02a6767b3d9c5d378b75
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-18 19:32:43 +0000

    add solution

--- 11-es6-module/02-node-and-npm-lesson/activities/06-Stu-Maths/solved/index.js
+++ 11-es6-module/02-node-and-npm-lesson/activities/06-Stu-Maths/solved/index.js
@@ -1,7 +1,41 @@
-// TODO: Import `maths.js`
+// Import `maths.js`
+const maths = require('./maths.js');
 
-// TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
+(function unitTests() {
+    console.log("Unit tests");
+    console.log("[");
+    console.log(`sum        : ${maths.sum(1,2) === 3 ? 'PASS' : 'FAIL'}`);
+    console.log(`difference : ${maths.difference(3,4) === -1 ? 'PASS' : 'FAIL'}`);
+    console.log(`product    : ${maths.product(3,4) === 12 ? 'PASS' : 'FAIL'}`);
+    console.log(`quotient   : ${maths.quotient(3,4) === 0.75 ? 'PASS' : 'FAIL'}`);
+    console.log("]\n");
+})();
 
-// TODO: Create a `switch` statement that accepts an `operation` parameter
-// and each `case` uses the corresponding `maths` method
-// to perform each math operation on the two numbers, `numOne` and `numTwo`
+(function doMaths() {
+
+    // Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
+    const [, , operation, numOne, numTwo] = process.argv;
+    let a = parseInt(numOne);
+    let b = parseInt(numTwo);
+
+    // Create a `switch` statement that accepts an `operation` parameter
+    // and each `case` uses the corresponding `maths` method
+    // to perform each math operation on the two numbers, `numOne` and `numTwo`
+    switch (operation) {
+        case 'sum':
+            console.log(`${a} + ${b} = ${maths.sum(a, b)}`);
+            break;
+        case 'difference':
+            console.log(`${a} - ${b} = ${maths.difference(a, b)}`);
+            break;
+        case 'product':
+            console.log(`${a} * ${b} = ${maths.product(a, b)}`);
+            break;
+        case 'quotient':
+            console.log(`${a} / ${b} = ${maths.quotient(a, b)}`);
+            break;
+        default:
+            console.log("Unknown operation");
+            break;
+    }
+})();
