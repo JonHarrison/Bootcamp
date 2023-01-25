25d037d963d402cb48f118fc545c5a8af146d854
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-25 20:21:42 +0000

    add implementation (thanks Marius)

--- 12-object-oriented-programming-module/02-tdd-lesson/activities/02-Stu-TDD/solved/algo.js
+++ 12-object-oriented-programming-module/02-tdd-lesson/activities/02-Stu-TDD/solved/algo.js
@@ -1,9 +1,20 @@
 function Algo() {}
 
-Algo.prototype.reverse = function(str) {};
+Algo.prototype.reverse = function(str) {
+    return str.split("").reverse().join("");
+};
 
-Algo.prototype.isPalindrome = function(str) {};
+Algo.prototype.isPalindrome = function(str) {
+    const reversed = this.reverse(str);
+    return str === reversed;
+};
 
-Algo.prototype.capitalize = function(str) {};
+Algo.prototype.capitalize = function(str) {
+    const words = str.split(" ");
+    const capitalizedWords = words.map((word) => {
+        return word[0].toUpperCase() + word.slice(1);
+        });
+    return capitalizedWords.join(" ");
+};
 
 module.exports = Algo;
