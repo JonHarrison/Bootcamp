9b3e6818daeb265ecfbed19baf4f890184af173b
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-26 21:43:39 +0000

    implement solution

--- 12-object-oriented-programming-module/03-es6-classes-lesson/activities/06-Stu-Multiple-Classes/solved/store.js
+++ 12-object-oriented-programming-module/03-es6-classes-lesson/activities/06-Stu-Multiple-Classes/solved/store.js
@@ -1,5 +1,9 @@
 class Store {
-  constructor() {}
+  constructor(name, stock) {
+    this.name = name;
+    this.stock = stock;
+    this.revenue = 0;
+  }
 
   printRevenue() {
     console.log(`The revenue so far is ${this.revenue}`);
@@ -7,6 +11,22 @@ class Store {
 
   welcome() {
     console.log(`Welcome to ${this.name}!`);
+  }
+
+  processProductSale(name) {
+    this.stock.filter(obj => { return obj.name === name; }).forEach(item => {
+      this.revenue += item.price;
+      if (item.count == 0) { console.log("Out of stock"); }
+      else {
+        item.count -= 1;
+      }
+    })
+  }
+
+  replenishStock(name, count) {
+    this.stock.filter(obj => { return obj.name === name; }).forEach(item => {
+      item.count += count;
+    })
   }
 }
 
