0d087a1af728ba659501c9914b2d08682fc47cf2
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-31 20:41:59 +0000

    implement HelloBootstrap component

---
+++ 13-react-module/01-intro-react-lesson/04-Stu-HelloBootstrap/solved/src/components/HelloBootstrap.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron">
+            <div className="p-5 mb-4 bg-light rounded-3">
+                <div className="container-fluid py-5">
+                    <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+                    <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+                    <button className="btn btn-primary btn-lg" type="button">Example button</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Card() {
+    return (
+        <div className="card" /*style="width: 18rem;"*/>
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Another link</a>
+            </div>
+        </div>
+    );
+}
+
+function HelloBootstrap() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Jumbotron />
+            <Card />
+        </div>
+    );
+}
+
+export default HelloBootstrap;
