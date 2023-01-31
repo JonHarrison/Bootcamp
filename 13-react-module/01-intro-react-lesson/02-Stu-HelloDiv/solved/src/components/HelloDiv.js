a1a7f62609d4afa852b5901faf57ab737b357c6e
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-31 19:47:10 +0000

    add solution

--- 13-react-module/01-intro-react-lesson/02-Stu-HelloDiv/solved/src/components/HelloDiv.js
+++ 13-react-module/01-intro-react-lesson/02-Stu-HelloDiv/solved/src/components/HelloDiv.js
@@ -1,5 +1,15 @@
 function HelloDiv() {
-  return null;
+  return (
+    <div>
+      <h1>Hello! My name is Jon</h1>
+      <h2>Things I like...</h2>
+      <ul>
+        <li>React</li>
+        <li>Javascript</li>
+        <li>GitHub</li>
+      </ul>
+    </div>
+  );
 }
 
 export default HelloDiv;
