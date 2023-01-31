92707feb1e6c74b0588e9144cc23c263e186d281
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-31 20:53:46 +0000

    add implementation

--- 13-react-module/01-intro-react-lesson/06-Stu-JSXVariables/solved/src/components/JSXVariables.js
+++ 13-react-module/01-intro-react-lesson/06-Stu-JSXVariables/solved/src/components/JSXVariables.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function JSXVariables() {
+  const name = 'Jon';
+  const options = [ 'is awesome', 'is amazing', 'is very powerful', "is the best thing we've done so far" ];
+
   return (
     <div className="main-container">
       <div className="container">
         <div className="jumbotron">
-          <h1>Hi! My name is (insert name here)</h1>
-          <h2>My name has (insert number of letters in name here) letters</h2>
-          <h2>I think React (insert thoughts about React here)</h2>
+          <h1>Hi! My name is {name}</h1>
+          <h2>My name has {name.length} letters</h2>
+          <h2>I think React { options[Math.floor(Math.random() * options.length)] }</h2>
         </div>
       </div>
     </div>
