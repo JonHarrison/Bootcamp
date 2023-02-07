fa14572947c48d4bdb00d041904382b046df4fca
Jon Harrison <jonathan_harrison@hotmail.com>
2023-02-07 20:37:04 +0000

    refactor API

--- 14-react-state-deployment-module/01-hooks-lesson/activities/04-Stu-useEffect/solved/src/utils/API.js
+++ 14-react-state-deployment-module/01-hooks-lesson/activities/04-Stu-useEffect/solved/src/utils/API.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the GitHub Jobs API
 
-export default {
-  searchTerms: function(query) {
+
+  function searchTerms(query) {
     return axios.get(
       "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
         query +
         "&limit=1&format=json&origin=*"
     );
   }
-};
+
+export default searchTerms;
